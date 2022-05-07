e6bac3a0bda9e29284452e9881f49d4d9bb66d94
Sam Timmins <stimmins@smu.edu>
2022-05-07 16:58:03 -0500

    uh oh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "\
+[general]\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
